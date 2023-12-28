f2c6f1b5159795db9cdbf0404fd2aeafbc262c58
Toopkarcher <ochir_erkhembayar@yahoo.com>
2023-12-28 12:23:33 +0000

    Remove comments

--- src/event.rs
+++ src/event.rs
@@ -10,31 +10,22 @@ use crossterm::event::{self, Event as CrosstermEvent, KeyEvent, MouseEvent};
 /// Terminal events.
 #[derive(Clone, Copy, Debug)]
 pub enum Event {
-    /// Terminal tick.
     Tick,
-    /// Key press.
     Key(KeyEvent),
-    /// Mouse click/scroll.
     Mouse(MouseEvent),
-    /// Terminal resize.
     Resize(u16, u16),
 }
 
-/// Terminal event handler.
 #[derive(Debug)]
 pub struct EventHandler {
-    /// Event sender channel.
     #[allow(dead_code)]
     sender: mpsc::Sender<Event>,
-    /// Event receiver channel.
     receiver: mpsc::Receiver<Event>,
-    /// Event handler thread.
     #[allow(dead_code)]
     handler: thread::JoinHandle<()>,
 }
 
 impl EventHandler {
-    /// Constructs a new instance of [`EventHandler`].
     pub fn new(tick_rate: u64) -> Self {
         let tick_rate = Duration::from_millis(tick_rate);
         let (sender, receiver) = mpsc::channel();
@@ -77,10 +68,6 @@ impl EventHandler {
         }
     }
 
-    /// Receive the next event from the handler thread.
-    ///
-    /// This function will always block the current thread if
-    /// there is no data available and it's possible for more data to be sent.
     pub fn next(&self) -> Result<Event> {
         Ok(self.receiver.recv()?)
     }
